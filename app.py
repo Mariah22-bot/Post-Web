2ba6553577ad7fc8f2a31163337f66a398eb159f
MARIA ISABEL <Mariah22-bot>
2026-02-21 13:52:21 -0300

    Configurando porta para o Render

--- app.py
+++ app.py
@@ -83,3 +83,8 @@ def delete(id):
     db.session.commit() # Salva a alteração definitivamente no banco de dados
     flash('"{}" foi apagado com sucesso!'.format(post.title)) # Mensagem de confirmação
     return redirect(url_for('index')) # Redireciona de volta para a página principal
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
